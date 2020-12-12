5fc290c7377e1849ae58a2198d73c0bb7536e52c
solucoestimg <75900739+solucoestimg@users.noreply.github.com>
2020-12-12 12:17:04 -0300

    Create atualizar-e-redirecionar.js

---
+++ atualizar-e-redirecionar.js
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Página de Teste</title>
+    <meta charset="utf-8"/>
+    <script type="text/javascript" src="script.js"></script>
+  </head>
+  <body>
+    <!-- esse é o comando redirect, vc será redirecionado para outra página ao clicar nele-->
+    <button onclick="window.location.href='http:/www.google.com.br/';">Ir para o Google</button>
+    
+    <!-- esse é o comando refrest, ele irá atualizar a sua página atual ao clicar nele-->
+    <button onclick="window.location.href= window.location.href  ">Atualizar a Página</button>
+  </body>
+</html>
+    
